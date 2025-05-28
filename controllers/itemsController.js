7b7e3db4de7f6293a558e721b1f22c64a230d9c2
Cameron Craig <sentrathis96@gmail.com>
2025-05-27 19:04:18 -0700

    Add itemService for looking up enabled items for a streamer

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,42 +1,11 @@
 const MasterItem = require('../models/masterItem');
-const StreamerItem = require('../models/streamerItem');
+const itemService = require('../services/itemService');
 
 module.exports = {
     get: async (req, res) => {
         const { streamerId } = req.query;
 
-        const enabledItems = await MasterItem.aggregate([
-            { $match: { enabled: true } },
-            {
-                $lookup: {
-                    from: 'streameritems',
-                    let: { masterItemId: '$itemId' },
-                    pipeline: [
-                        {
-                            $match: {
-                                $expr: {
-                                    $and: [
-                                        { $eq: ['$masterItemId', '$$masterItemId'] },
-                                        { $eq: ['$streamerId', streamerId] },
-                                        { $eq: ['$enabled', true] }
-                                    ]
-                                }
-                            }
-                        }
-                    ],
-                    as: 'streamerItems'
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    itemId: 1,
-                    name: 1,
-                    price: { $ifNull: [{ $arrayElemAt: ['$streamerItems.price', 0] }, null] },
-                    enabled: { $ifNull: [{ $arrayElemAt: ['$streamerItems.enabled', 0] }, false] }
-                }
-            }
-        ]);
+        const enabledItems = await itemService.getEnabledItemsForStreamer(streamerId);
 
         res.status(200).json(enabledItems);
     },
