9b1ffd4af86c101b729af28c11699ad68d96963c
Cameron Craig <sentrathis96@gmail.com>
2025-05-27 18:39:50 -0700

    Properly handle redeeming tokens

--- controllers/tokensController.js
+++ controllers/tokensController.js
@@ -2,7 +2,7 @@ const Token = require("../models/token")
 const { getCurrentViewer } = require("../middlewares/viewerAuth")
 const { broadcastEvent } = require("../services/eventService")
 const Streamer = require("../models/streamer")
-
+const StreamerItem = require("../models/streamerItem")
 
 module.exports = {
     get: async (req, res) => {
@@ -24,20 +24,36 @@ module.exports = {
     },
     redeem: async (req, res) => {
         const { tokenIds, itemId, streamerId, xCoord } = req.body
-        const tokens = await Token.find({ _id: { $in: tokenIds } })
-
-        const redeemedAt = new Date();
-
-        // Update all tokens with redeemedAt timestamp
-        await Promise.all(tokens.map(token => {
-            token.redeemedAt = redeemedAt
-            return token.save()
-        }))
-        
-        
+        const tokens = await Token.find({ _id: { $in: tokenIds }, redeemedAt: null })
+        const item = await StreamerItem.findOne({
+            masterItemId: itemId,
+            streamerId: streamerId,
+            enabled: true
+        })
         const streamer = await Streamer.findOne({ "twitchProfile.id": streamerId })
 
-        broadcastEvent(streamer.channelId,{
+        if (tokens.length < item.price) {
+            return res.status(400).json({ error: `Not enough tokens. You need ${item.price} tokens to spawn ${item.name}` })
+        }
+
+        if (!item) {
+            return res.status(400).json({ error: 'Item not found' })
+        }
+
+        if (!streamer) {
+            return res.status(400).json({ error: 'Streamer not found' })
+        }
+
+        await Token.updateMany({
+            _id: { $in: tokenIds }
+        }, {
+            $set: {
+                redeemedAt: new Date(),
+                redeemedFor: item.masterItemId
+            }
+        })
+
+        broadcastEvent(streamer.channelId, {
             type: 'spawn_item',
             data: {
                 itemId,
