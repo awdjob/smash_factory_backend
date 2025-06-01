ff305a769af8424f1e174c2935bcad059a1d497f
Johnny Chewnami <johnny@livearcade.io>
2025-05-31 23:45:06 -0700

    change signups controller to signup controller

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = async (req, _, next) => {
     } else if (req.get("X-Auth-Source") === "client" || req.path.includes("/events")) {
         // await streamerAuth(req, _, next)
         next()
-    } else if (req.path.includes("/auth") || req.path.includes("/webhook/twitch") || req.path.includes("/signups")) {
+    } else if (req.path.includes("/auth") || req.path.includes("/webhook/twitch") || req.path.includes("/signup")) {
         next()
     } else {
         const error = new Error("Invalid Auth Source")
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { broadcastEvent, addClient, removeClient } = require('./services/eventSer
 const Streamer = require('./models/streamer');
 const tokenService = require('./services/twitchChatTokenService');
 const itemsController = require('./controllers/itemsController');
-const signUpController = require('./controllers/signUpsController');
+const signUpController = require('./controllers/signupController');
 
 const app = express();
 app.use(cors());
@@ -59,7 +59,7 @@ app.get('/events', async (req, res) => {
 app.get('/auth', tokenService.handleOAuthCallback);
 app.post('/webhook/twitch', twitchWebhookController.process);
 app.get('/items', itemsController.get);
-app.post('/signups', signUpController.post);
+app.post('/signup', signUpController.post);
 
 if (process.env.NODE_ENV === 'test') {
     app.get('/viewer_test', (req, res) => {
